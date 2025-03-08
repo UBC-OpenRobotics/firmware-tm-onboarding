d4c38755982feb1ce9883d9ec27062d94c176000
ih8luke <lukehuang27@Gmail.com>
2025-03-08 11:50:12 -0800

    Update sensor.h

---
+++ Modules/inc/sensor.h
@@ -0,0 +1,19 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <stdint.h>
+#include "usart.h"
+
+#define TFMINI_FRAME_HEADER 0x59
+#define TFMINI_FRAME_SIZE 9
+
+typedef struct {
+    uint16_t distance;
+    uint16_t strength;
+    uint8_t temperature;
+} TFminiData;
+
+void sensor_init(void);
+int sensor_read(TFminiData *data);
+
+#endif // SENSOR_H
