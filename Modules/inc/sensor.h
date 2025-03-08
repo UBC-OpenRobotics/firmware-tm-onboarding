55aad5cd22e1a6b6a3ee0c2df843fc6fe07191db
kathrynezong <90151978+kathrynezong@users.noreply.github.com>
2025-03-07 21:58:00 -0800

    Update sensor.h

---
+++ Modules/inc/sensor.h
@@ -0,0 +1,25 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+#include "stm32h7xx_hal.h"
+#include "stm32h723xx.h"
+
+namespace UltrasonicSensor
+{
+    class Sensor {
+    public:
+        Sensor(GPIO_TypeDef* trigPort, uint16_t trigPin,
+               GPIO_TypeDef* echoPort, uint16_t echoPin);
+
+        uint16_t readPulse();
+        float getDistanceCm();
+
+    private:
+        GPIO_TypeDef* trigPort;
+        uint16_t trigPin;
+        GPIO_TypeDef* echoPort;
+        uint16_t echoPin;
+    };
+}
+
+#endif 
